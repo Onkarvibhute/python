e70ea9d4a4d4a0ee3ff017677e297e3ff9600b7e
Onkarvibhute <vibhuteonkar2002@gmail.com>
2026-05-10 21:45:59 +0530

    for_loop

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+#for loop
+'''
+for i in range(5):
+    print('on this itr i->'+ str(i));
+'''
+
+'''for i in range(1,5):
+    print(i);
+'''
+
+'''for i in range(0,10,2):
+    print(i);
+'''
+
+'''for i in range(5,0,-1):
+    print(i);
+'''
+
