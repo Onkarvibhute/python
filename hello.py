3b6a25e9596d21dc613f53870e5e07acad216def
onkar-mdlz <Onkar.Vibhute@mdlz.com>
2026-05-10 09:29:41 +0530

    may_10

---
+++ hello.py
@@ -0,0 +1,9 @@
+print('hello world')
+print('whats ur name');
+my_name=input('>')
+print('my name is ' + my_name);
+print('len of ur name is')
+print(my_name +'-> ' +str(len(my_name)));
+print('whats ur age');
+my_age=input('>');
+print('user_name is '+ my_name +'\nuser_age is '+ my_age);
